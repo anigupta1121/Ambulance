e8dd5bbf44921630e0cabb8e8019e18a697c00a8
Ashish <dynamiclink123@gmail.com>
2017-10-29 02:02:16 +0530

    Location

---
+++ app/src/main/java/com/ambulance/MapsActivity.java
@@ -0,0 +1,202 @@
+package com.ambulance;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    double lati, longi;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0&&grantResults[0]== PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER,locationListener,null);
+            Location mylo= locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            myupdate(mylo);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                myupdate(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        getLocation();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+
+        LatLng myLoc = new LatLng(lati, longi);
+
+
+        mMap.addMarker(new MarkerOptions().position(myLoc).title("Your Location"));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLoc,15));
+    }
+
+    void myupdate(Location location)
+    {
+        lati=location.getLatitude();
+        longi=location.getLongitude();
+         Log.i("MyLoc",location.toString());
+
+
+    }
+
+
+    public void getLocation() {
+
+        if (Build.VERSION.SDK_INT < 23) {
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER,locationListener,null);
+            Location mylo= locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            myupdate(mylo);
+        } else {
+
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String s[] = {android.Manifest.permission.ACCESS_FINE_LOCATION};
+                ActivityCompat.requestPermissions(this, s, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER,locationListener,null);
+               Location mylo= locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                Log.i("MyLocal",mylo.toString());
+                myupdate(mylo);
+            }
+        }
+    }
+
+    public void addAmbulance(View view)
+    {
+        // Create custom dialog object
+        final Dialog dialog = new Dialog(MapsActivity.this);
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.dialog_layout);
+        // Set dialog title
+        dialog.setTitle("Add Ambulance");
+
+        // set values for custom dialog components - text, image and button
+        EditText e = (EditText) dialog.findViewById(R.id.ambulanceName);
+
+        dialog.show();
+
+        Button addButton = (Button) dialog.findViewById(R.id.add);
+        // if decline button is clicked, close the custom dialog
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add info to firebase
+            }
+        });
+
+        Button placeButton = (Button) dialog.findViewById(R.id.cancel);
+        // if decline button is clicked, close the custom dialog
+        placeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Choose Place
+            }
+        });
+
+        Button declineButton = (Button) dialog.findViewById(R.id.cancel);
+        // if decline button is clicked, close the custom dialog
+        declineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Close dialog
+                dialog.dismiss();
+            }
+        });
+    }
+}
